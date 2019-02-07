5ef95be9ed519bc397c9f8b92a137d340a199264
Syed Hussain Ather <shussainather@gmail.com>
2019-02-07 14:10:26 -0500

    fixed space issue

--- mygene.py
+++ mygene.py
@@ -11,7 +11,7 @@ with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fa", "r") as file:
             gene = line.split(" ")[3].split(":")[1]
             gene_name.append(line.split(" ")[3].split(":")[1])
             ens.append(line.split(" ")[0].replace(">", ""))
-            o.write(">" + str(gene) + "".join(line.split(" ")[1:]))
+            o.write(">" + str(gene) + " " + " ".join(line.split(" ")[1:]))
         else:
             o.write(str(line))
 
