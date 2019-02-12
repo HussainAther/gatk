e18c53ddf0ac59ead9bb21f66100ea100d792577
Syed Hussain Ather <shussainather@gmail.com>
2019-02-11 19:48:56 -0500

    using ENSDARG

--- fixname.py
+++ fixname.py
@@ -3,7 +3,7 @@ o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed.fa", "w")
 with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fa", "r") as file:
     for line in file:
         if line.startswith(">"):
-            gene_name = line.split(" ")[0].replace(">", "")
-            o.write(" ".join(line.split(" ")[:3]) + " gene:" + gene_name + " " + " ".join(line.split(" ")[4:]))
+            gene_name = line.split(" ")[3].replace("gene:", "")
+            o.write(">" + gene_name + " " + " ".join(line.split(" ")[1:3]) + " gene:" + gene_name + " " + " ".join(line.split(" ")[4:]))
         else:
             o.write(line)
