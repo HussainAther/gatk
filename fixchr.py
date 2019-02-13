152263a70223dbd3f83116d13d19f7763b92f353
Syed Hussain Ather <shussainather@gmail.com>
2019-02-12 21:14:07 -0500

    fix space

--- fixchr.py
+++ fixchr.py
@@ -4,6 +4,6 @@ with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed.fa", "r") as fil
     for line in file:
         if line.startswith(">"):
             chromosome = line.split(":")[1]
-            o.write(">" + chromosome + " ".join(line.split(" ")[1:]))
+            o.write(">" + chromosome + " "+ " ".join(line.split(" ")[1:]))
         else:
             o.write(line)
