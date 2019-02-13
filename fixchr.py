493a74285d45fea8e583ee9c62068d185ed93738
Syed Hussain Ather <shussainather@gmail.com>
2019-02-12 21:13:27 -0500

    initial commit

---
+++ fixchr.py
@@ -0,0 +1,9 @@
+o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed2.fa", "w")
+
+with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed.fa", "r") as file:
+    for line in file:
+        if line.startswith(">"):
+            chromosome = line.split(":")[1]
+            o.write(">" + chromosome + " ".join(line.split(" ")[1:]))
+        else:
+            o.write(line)
