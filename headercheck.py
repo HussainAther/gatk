8f46f08b507c3d1fc31288bd4510ce5393feb6ff
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 21:19:21 -0500

    chr check

--- headercheck.py
+++ headercheck.py
@@ -1,4 +1,12 @@
 chr_list = []
 
-with open("/data/athersh/gatk/Undetermined_S0_L001-1.sorted.header.sam", "r"):
-    for line in file
+o = output=("/data/athersh/gatk/mapped/Undetermined_S0_L001-1.sorted.newheader.sam", "w")
+
+with open("/data/athersh/gatk/mapped/Undetermined_S0_L001-1.sorted.header.sam", "r") as file:
+    for line in file:
+        if "chr" in line:
+            chro = line.split("\t")[1].replace("SN:", "")  
+            if chro not in chr_list: 
+                chr_list.append(chro)
+
+print(chr_list)
