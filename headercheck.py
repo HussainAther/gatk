a1f713c80478147f1683e515f578d8fe31c8b7ab
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 21:22:42 -0500

    write unique headers

--- headercheck.py
+++ headercheck.py
@@ -1,12 +1,14 @@
 chr_list = []
 
-o = output=("/data/athersh/gatk/mapped/Undetermined_S0_L001-1.sorted.newheader.sam", "w")
+o = open("/data/athersh/gatk/mapped/Undetermined_S0_L001-1.sorted.newheader.sam", "w")
 
 with open("/data/athersh/gatk/mapped/Undetermined_S0_L001-1.sorted.header.sam", "r") as file:
     for line in file:
         if "chr" in line:
             chro = line.split("\t")[1].replace("SN:", "")  
-            if chro not in chr_list: 
+            if chro not in chr_list:
+                o.write("\t".join(line.split("\t")[:-1]))
+                o.write("\n")
                 chr_list.append(chro)
-
-print(chr_list)
+        else:
+            o.write(line)
