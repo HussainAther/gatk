c2301323039f9c2038f1b56ad8b31444387e05fc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 22:49:11 -0400

    initial commit

---
+++ fixcontigheader.py
@@ -0,0 +1,12 @@
+
+headerlist = []
+
+o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed4.fa", "w") 
+
+with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed3.fa", "r") as file:
+    for line in file:
+        if line.startswith(">"):
+            length = int(line.split(":")[3]) - int(line.split(":")[2])
+            o.write(line.split(" ")[0] + "." + str(length) + " " + " ".join(line.split(" ")[1:]))
+        else:
+            o.write(line)
