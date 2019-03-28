5c943022ed74060a5b6bd946c00ee99f441a7827
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 23:18:18 -0400

    one more int

--- fixcontigheader.py
+++ fixcontigheader.py
@@ -6,7 +6,7 @@ o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed4.fa", "w")
 with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed3.fa", "r") as file:
     for line in file:
         if line.startswith(">"):
-            length = int(line.split(":")[3]) - int(line.split(":")[2])
+            length = int(line.split(":")[3]) - int(line.split(":")[2]) - 1
             o.write(line.split(" ")[0] + "." + str(length) + " " + " ".join(line.split(" ")[1:]))
         else:
             o.write(line)
