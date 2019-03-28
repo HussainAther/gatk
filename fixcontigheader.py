2c44babbfeb5c730a3ce2ec259ae19a80bbce7d7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-28 00:34:53 -0400

    3

--- fixcontigheader.py
+++ fixcontigheader.py
@@ -1,9 +1,9 @@
 
 headerlist = []
 
-o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed4.fa", "w") 
+o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed3.fa", "w") 
 
-with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed3.fa", "r") as file:
+with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed2.fa", "r") as file:
     for line in file:
         if line.startswith(">"):
             length = int(line.split(":")[3]) - int(line.split(":")[2]) - 1
--- fixname.py
+++ fixname.py
@@ -3,8 +3,6 @@ o = open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fixed.fa", "w")
 with open("/data/athersh/gatk/Danio_rerio.GRCz10.cdna.all.fa", "r") as file:
     for line in file:
         if line.startswith(">"):
-#            gene_name = line.split(" ")[3].replace("gene:", "")
-#            o.write(">" + gene_name + " " + " ".join(line.split(" ")[1:3]) + " gene:" + gene_name + " " + " ".join(line.split(" ")[4:]))
             gene_name = line.split(" ")[0].replace(">", "")
             o.write(">" + gene_name + " " + " ".join(line.split(" ")[1:3]) + " gene:" + gene_name + " " + " ".join(line.split(" ")[4:]))
         else:
